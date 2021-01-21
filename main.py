0f914bb4dc35d803d7292935a5f9f54c1484a1ca
ReidBarbeln2022 <814684@mystma.org>
2021-01-21 16:09:51 +0000

    factorial try 1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+def factorial(x):
+    
+    total = 1
+    
+    
+    for i in range (1, x + 1):
+        total = total * i
+
+
+    return total
+
+print (factorial(5))
+
